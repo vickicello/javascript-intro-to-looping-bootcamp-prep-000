a00a8ad73cd79ec4730d6a133cc6c453dfa61f81
Vicki Hamm <vicki.cello@gmail.com>
2018-04-11 01:48:40 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,11 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-  if (i === 1) {
-    console.log("I am " + i + "strange loop.")
-  }
-  else {
-    console.log("I am ${i} strange loops.")
+    if (i === 1) {
+      console.log("I am " + i + "strange loop.")
+    }
+    else {
+      console.log("I am ${i} strange loops.")
+    }
   }
 }
-}
+
