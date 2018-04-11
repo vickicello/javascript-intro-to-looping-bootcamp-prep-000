54a5db8bb902211cae7e68df59b2adbc02f67570
Vicki Hamm <vicki.cello@gmail.com>
2018-04-11 01:45:26 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,8 +1,7 @@
 function forLoop(array) {
-
-for (let i = 0; i < 25; i++) {
-console.log("I am ${i} strange loops.");
-if (i > 1) {console.log("I am ${i} strange loops.")}
-else {console.log("I am 1 strange loop.")}
+  for (let i = 0; i < 25; i++) {
+  console.log("I am ${i} strange loops.");
+  if (i > 1) {console.log("I am ${i} strange loops.")}
+  else {console.log("I am 1 strange loop.")}
 }
 
