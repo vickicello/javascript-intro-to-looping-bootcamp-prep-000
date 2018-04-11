2304b00d04bf668b226f7c3b17875a8c30ad493d
Vicki Hamm <vicki.cello@gmail.com>
2018-04-11 01:39:24 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 for (let i = 0; i < 25; i++) {
-if (i >1) console.log("I am ${} strange loops.");
-else console.log("I am ${} strange loop.");
+if (i > 1) {console.log("I am ${} strange loops.")}
+else {console.log("I am ${} strange loop.")}
 }
 
