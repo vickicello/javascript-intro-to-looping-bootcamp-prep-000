bdcc0e81ba4bc6364111da32cb19cbce80b8c64a
Vicki Hamm <vicki.cello@gmail.com>
2018-04-11 01:48:17 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,6 +4,7 @@ function forLoop(array) {
     console.log("I am " + i + "strange loop.")
   }
   else {
-    console.log("I am ${i} strange loops.")}
+    console.log("I am ${i} strange loops.")
+  }
 }
-
+}
